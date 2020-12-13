e18d155b1e4241fa909bae4929e4216efcc5234f
luiznd <luiznd@hotmail.com>
2020-12-13 14:47:46 -0300

    Comitando projeto de etl Go

---
+++ files/tmp_compracliente.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE public.sq_pk_tmp_compracliente START 1;
+
+CREATE TABLE tmp_compracliente (
+    id_compracliente BIGINT NOT NULL DEFAULT nextval('public.sq_pk_tmp_compracliente'),
+    CPF VARCHAR(20) NOT NULL,
+    private VARCHAR(20),
+	incompleto VARCHAR(20),
+	data_ultima_compra date,
+	compra_ticket_medio float,
+	ticket_ultima_compra float,
+	loja_mais_frequente VARCHAR(20),
+	loja_ultima_compra VARCHAR(20),
+	data_criacao timestamp,
+	data_modificacao timestamp,
+	CONSTRAINT pk_tmp_compracliente PRIMARY KEY (id_compracliente)
+)
